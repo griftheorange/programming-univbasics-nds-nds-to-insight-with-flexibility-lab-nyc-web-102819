9f8540bc0e23f05f58530b9820ac9a59635e3d7f
Griffin Poole <griffinpoole5@gmail.com>
2019-10-16 00:25:47 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -56,16 +56,7 @@ def gross_per_studio(collection)
 end
 
 def movies_with_directors_set(source)
-  # GOAL: For each director, find their :movies Array and stick it in a new Array
-  #
-  # INPUT:
-  # * source: An Array of Hashes containing director information including
-  # :name and :movies
-  #
-  # RETURN:
-  #
-  # Array of Arrays containing all of a director's movies. Each movie will need
-  # to have a :director_name key added to it.
+  pp source
 end
 
 # ----------------    End of Your Code Region --------------------
